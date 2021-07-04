3acbc9cac3982ff79b75588e5e73fc519333176f
kimjongkeun <jongkeun@godo.co.kr>
2021-07-04 12:35:18 +0900

    Add model

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
+import model from './model/index.js'
+
 $(function(){
   console.log(jQuery)
   console.log($)
+
+  console.log(model.data)
 })
---
+++ model/index.js
@@ -0,0 +1,5 @@
+class Model {
+  data = 'hello model'
+}
+
+export default new Model()
